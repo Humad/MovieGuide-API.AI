c68b7d4c59244ae027999d0f4360a869b2629d51
Humad <humad@live.com>
2017-01-07 18:51:21 +0500

    added testing functions

--- index.js
+++ index.js
@@ -4,17 +4,49 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 3000;
 
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+function howToUse(req, res){
+    console.log('This is the howToUse function');
+    sendResponse(res);
+};
+
+function sendResponse(res){
+    var response = {
+        "speech": "You can ask me to help you with Movies!",
+        "displayText": "You can ask me to help you with Movies!",
+        "data": {},
+        "contextOut":{},
+        "source":""
+    };
+
+    res.json(response);
+}
+
+var actionMap = new Map();
+actionMap.set('howToUse', howToUse);
+
 app.get('/', function(req, res){
-    res.send('This is a webhook for the MovieBuff API. Send a POST request');
+    res.send('This is the webhook for MovieBuff. Send a POST request');
 });
 
 app.post('/', function(req, res){
-    res.send(req.body);
-    console.log(req.body);
+    if (!req.body.result){
+        console.log('Incorrect JSON object send');
+    } else {
+        var actionName = req.body.result.action;
+        if (actionMap.has(actionName)){
+            actionMap.get(actionName)(req, res);
+        } else {
+            sendResponse(res);
+        }
+    }
 });
+
+
 
 app.listen(port, function(){
     console.log('App listening on ' + port);
