bac1fc1e0231f358c6180dcb325f0d45a06786d2
Humad <humad@live.com>
2017-01-08 19:43:32 +0500

    added director function

--- index.js
+++ index.js
@@ -114,7 +114,8 @@ function movieCast(req, res){
 };
 
 function generateMovieCastResponse(req, res, body){
-    var numMovies = (body.results[0].known_for.length > 3 ? 3 : body.results[0].known_for.length);
+    var movies = body.results[0].known_for;
+    var numMovies = (movies.length > 3 ? 3 : movies.length);
     var actor = req.body.result.parameters.actorName[0];
 
     var speechText;
@@ -122,11 +123,10 @@ function generateMovieCastResponse(req, res, body){
         speechText = "I could not find any movies starring"
                     + actor;
     } else {
-        var movies = body.results[0].known_for;
         speechText = movies[0].title;
         for (var i = 1; i < numMovies; i++){
             speechText += (i !== 1 && i === numMovies - 1 ? " and " : "");
-            speechText += (" " + movies[i].title);
+            speechText += (", " + movies[i].title);
         }
     }
 
@@ -147,15 +147,64 @@ function generateMovieCastResponse(req, res, body){
 // post: sends the names of movies directed by the director requested by the user
 function movieDirector(req, res){
     console.log('This is the movie director function');
-    var response = {
-        "speech": "You asked for details about " + req.body.result.parameters.directorName[0],
-        "displayText": "You asked for details about " + req.body.result.parameters.directorName[0],
+    var director = req.body.result.parameters.directorName;
+
+    var requestOptions = {
+        url: "https://api.themoviedb.org/3/search/person",
+        method: "GET",
+        json: {},
+        qs: {
+            api_key: API_KEY,
+            language: "en-US",
+            query: director,
+            page: 1,
+            include_adult: false
+        }
+    };
+
+    request(requestOptions, function(err, response, body){
+        console.log('Request sent to api');
+        if (err || res.statusCode !== 200){
+            console.log('Error from api: ' + err);
+            res.status(400);
+        } else {
+            console.log('Request successful');
+            console.log(body);
+            generateMovieDirectorResponse(req, res, body);
+        }
+    });
+};
+
+// to-do: this is a redundant function; find a way to merge with moviecastresponse
+function generateMovieDirectorResponse(req, res, body){
+    var movies = body.results[0].known_for;
+    var numMovies = (movies.length > 3 ? 3 : movies.length);
+    var director = req.body.result.parameters.directorName;
+
+    var speechText;
+    if (numMovies === 0) {
+        speechText = "I could not find any movies directed by "
+                    + director;
+    } else {
+        speechText = movies[0].title;
+        for (var i = 1; i < numMovies; i++){
+            speechText += (i !== 1 && i === numMovies - 1 ? " and " : "");
+            speechText += (", " + movies[i].title);
+        }
+    }
+
+    speechText = director + " has directed movies such as " + speechText;
+
+    var speechResponse = {
+        "speech": speechText,
+        "displayText": speechText,
         "data": {},
         "contextOut":[],
         "source":""
     };
-    sendResponse(res, response);
-};
+
+    sendResponse(res, speechResponse);
+}
 
 // pre: takes request and response objects as parameters
 // post: gives a movie recommendation to the user
