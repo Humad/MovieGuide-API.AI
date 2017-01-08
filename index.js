2ba9c9fbd275fec238d92e73953b25cb6ba53625
Humad <humad@live.com>
2017-01-08 14:00:07 +0500

    added moviedb api support

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
+var request = require('request');
 var port = process.env.PORT || 3000;
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+let API_KEY = "7532c5a122204ba501be1d49beed6b53";
 
 // pre: takes request and response objects as parameters
 // post: sends instructions to the user about how to use the agent
@@ -18,15 +21,40 @@ function howToUse(req, res){
 // pre: takes request and response objects as parameters
 // post: sends details regarding the movie the user asked for
 function movieDetails(req, res){
-    console.log('This is the movie details function');
-    var response = {
-        "speech": "You asked for details about " + req.body.result.parameters.movieName,
-        "displayText": "You asked for details about " + req.body.result.parameters.movieName,
-        "data": {},
-        "contextOut":[],
-        "source":""
+    console.log('Request received for movie details');
+
+    var requestOptions = {
+        url: "https://api.themoviedb.org/3/search/movie",
+        method: "GET",
+        json: {},
+        qs: {
+            api_key: API_KEY,
+            language: "en-us",
+            query: req.body.result.parameters.movieName,
+            page: 1,
+            include_adult:false
+        }
     };
-    sendResponse(res, response);
+
+    request(requestOptions, function(err, response, body){
+        console.log('Request sent to api');
+        if (err || res.statusCode !== 200){
+            console.log('Error from api: ' + err);
+            res.status(400);
+        } else {
+            console.log('Request successful');
+            var movieData = body.results[0];
+            var speechResponse = {
+                "speech": "You asked for details about " + movieData.title,
+                "displayText": "You asked for details about " + movieData.title,
+                "data": {},
+                "contextOut":[],
+                "source":""
+            };
+
+            sendResponse(res, speechResponse);
+        }
+    };
 };
 
 // pre: takes response and request objects as parameters
@@ -43,6 +71,8 @@ function movieCast(req, res){
     sendResponse(res,response);
 };
 
+// pre: takes request and response objects as parameters
+// post: sends the names of movies directed by the director requested by the user
 function movieDirector(req, res){
     console.log('This is the movie director function');
     var response = {
@@ -53,9 +83,21 @@ function movieDirector(req, res){
         "source":""
     };
     sendResponse(res, response);
-}
+};
 
-
+// pre: takes request and response objects as parameters
+// post: gives a movie recommendation to the user
+function movieRecommendation(req, res){
+    console.log('This is the movie recommendation function');
+    var response = {
+        "speech": "You asked for a recommendation",
+        "displayText": "You asked for a recommendation",
+        "data": {},
+        "contextOut":[],
+        "source":""
+    };
+    sendResponse(res, response);
+};
 
 // pre: takes the server response object and a custom response object
 // post: sends the custom response object through the server response object as JSON
@@ -71,6 +113,7 @@ actionMap.set('howToUse', howToUse);
 actionMap.set('movie.details', movieDetails);
 actionMap.set('movie.cast', movieCast);
 actionMap.set('movie.director', movieDirector);
+actionMap.set('movie.recommendation', movieRecommendation);
 
 
 app.get('/', function(req, res){
